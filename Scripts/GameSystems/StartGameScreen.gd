5044ca034ff72381acb92c81d80e39c59e8cedf7
ToxicMask <tomas.carciofi@gmail.com>
2022-11-07 14:10:20 -0300

    Dummy World Screen

--- Scripts/GameSystems/StartGameScreen.gd
+++ Scripts/GameSystems/StartGameScreen.gd
@@ -3,5 +3,5 @@ class_name StarGameScreen
 
 func _next_scene():
 	$NewGameManager._set_new_game_data()
-	get_tree().call_group("Main", "_change_world_scene", "TEST1")
+	get_tree().call_group("Main", "_change_world_scene", "World1")
 	pass
--- Scripts/GameSystems/WorldScreen.gd
+++ Scripts/GameSystems/WorldScreen.gd
@@ -5,3 +5,9 @@ class_name WorldScreen
 func _enter_tree():
 	self.add_to_group("DeleteOnChangeScene")
 	pass
+
+func _ready():
+	var main = get_parent() as Main
+
+	main._add_tape_recorder_hud()
+
