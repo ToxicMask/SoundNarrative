db923ed49e8ce7f5f97bb2a76ec7545244156d06
ToxicMask <tomas.carciofi@gmail.com>
2022-11-20 19:18:05 -0300

    Pulse Shader

---
+++ Nodes/_Debug/_DebugSchockScreen.gd
@@ -0,0 +1,19 @@
+extends ColorRect
+
+
+func _normalize_screen_position(_vec :Vector2) -> Vector2:
+	return Vector2( lerp(-0.35 , 1.35, _vec.x / 1024.0), 1.0 -(_vec.y / 600.0)  )
+
+
+
+func _input(event):
+
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and (event.pressed and not event.is_echo() ):
+			print("PRINT")
+			var pos =  _normalize_screen_position( get_global_mouse_position() )
+			#print(pos)
+			material.set_shader_param( "pulseCenter" , pos)
+			$Anim.stop()
+			$Anim.play("_DebugPulse")
+	pass
