e05276e197c02ddad5e7612034c4019ef68dccbc
ToxicMask <42303435+ToxicMask@users.noreply.github.com>
2022-11-23 17:35:21 -0300

    GameWorlds Rework + Scene Actions Animations

--- Scripts/GameSystems/Subworld/SubWorld1x3.gd
+++
@@ -1,8 +0,0 @@
-extends SubWorld
-
-export (Texture) var revealed_texture : Texture = null
-
-func _reveal_table_content():
-	$WorldRender.texture = revealed_texture;
-	$PickUpTape.show()
-	pass
--- Scripts/GameSystems/Subworld/SubWorld1x4.gd
+++
@@ -1,8 +0,0 @@
-extends SubWorld
-
-export (Texture) var open_texture : Texture = null
-
-func _open_door():
-	$WorldRender.texture = open_texture;
-	$GoNextSceneInteractable.show()
-	pass
