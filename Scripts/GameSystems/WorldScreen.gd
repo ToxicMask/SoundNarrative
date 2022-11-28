dd182cf442b4dde2202827468c92698dfd274cb0
ToxicMask <tomas.carciofi@gmail.com>
2022-11-28 03:47:23 -0300

    Modeling v0

--- Scripts/GameSystems/WorldScreen.gd
+++ Scripts/GameSystems/WorldScreen.gd
@@ -1,7 +1,6 @@
 extends Node2D
 class_name WorldScreen
 
-onready var sound_control := $SoundController 
 
 func _enter_tree():
 	self.add_to_group("DeleteOnChangeScene")
@@ -13,3 +12,6 @@ func _go_to_next_scene(scene_id : String):
 
 func _get_tape_recorder_controller():
 	return get_node_or_null("TapeRecorderController")
+
+func _get_sound_controller():
+	return get_node_or_null("SoundController ")
